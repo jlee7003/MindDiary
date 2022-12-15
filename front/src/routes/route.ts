938a41d47c7715675714383e439cdcb4900bae11
jlee7003 <jlee7003@naver.com>
2022-12-15 21:04:26 +0900

    FE: build FIX

---
+++ front/src/routes/route.ts
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface route {
+    path: string;
+}
+
+interface routeWrap {
+    [key: string]: route;
+}
+
+export const ROUTES: routeWrap = {
+    LOGIN: {
+        path: "/",
+    },
+};
+export const ROUTES_LIST: route[] = Object.values(ROUTES);
